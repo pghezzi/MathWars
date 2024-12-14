bdbca9771007203c99d095bb308ffde76d50ef3c
Yusuf Elley <yelley2001@gmail.com>
2024-12-14 15:00:08 -0500

    Add pausing functionality

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -10,18 +10,26 @@ public class Pause : MonoBehaviour
     public void handlePause()
     {
         Debug.Log("PAUSE"); 
+        // This line will pause anything that relies on time in Unity
+        // If we want to still do something when timeScale is 0 we can use unscaledTime or realtime
+        // Audio has a pause funciton if we need to pause it
+        // This will still register user inputs so if we find a weird bug where after being paused
+        // many actions are being performed we may want to add a isPaused flag across our scripts
+        Time.timeScale = 0;
         Instantiate(pauseScreen);
     }
 
     public void handleResume()
     {
         Debug.Log("RESUME");
+        Time.timeScale = 1;
         Destroy(pauseScreen);
     }
     
     public void handleQuitLevel()
     {
         SceneManager.LoadScene("LevelMenu");
+        Time.timeScale = 1;
     }
 
 }
