7aaef2c13e7308774dc4dd1d5c1522615224ee6b
pghezzi <100241979+pghezzi@users.noreply.github.com>
2024-12-15 16:00:36 -0500

    Added Tower Placement ui to genral ui to allow for proper initialization

--- Assets/Tower/Scripts/TowerCreateButton.cs
+++ Assets/Tower/Scripts/TowerCreateButton.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class TowerCreateButton : MonoBehaviour
 {
     private TowerPlacingUI tpui;
+    private InfoPanelManager infoPanelManager;
 
     void Start()
     {
         tpui = gameObject.transform.parent.parent.GetComponent<TowerPlacingUI>();
+        infoPanelManager = GameObject.Find("User Interface(Clone)").gameObject.transform.GetChild(1).GetComponent<InfoPanelManager>();
     }
 
     public void onClick(GameObject towerToCreate)
     {
-        Debug.Assert(towerToCreate != null);
-        Debug.Log("hey");
         float[] pos = tpui.CurrentPos();
         GameObject tower = Instantiate(towerToCreate);
         tower.transform.localScale = new UnityEngine.Vector3(10.0f, 10.0f, 10.0f);
         tower.transform.position = new UnityEngine.Vector3(pos[0], 0, pos[1]);
         tpui.deactivate();
+        infoPanelManager.gainCoins(10);
     }
 
 }
