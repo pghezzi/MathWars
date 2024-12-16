cff35e5dea3b12304b64a0edd2caaafc909f69fb
Ahmad Shahrour <ashahrourr@gmail.com>
2024-12-16 15:24:47 +0300

    Update enemy.cs
    enemy dies when reached endzone

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -137,10 +137,11 @@ public class Enemy : MonoBehaviour
         if (
             (transform.position.x <= EndPoint.position.x + 1f && transform.position.x >= EndPoint.position.x - 1f)
             &&
-            (transform.position.y <= EndPoint.position.y + 1f && transform.position.y >= EndPoint.position.y - 1f)
-            )
+            (transform.position.z <= EndPoint.position.z + 1f && transform.position.z >= EndPoint.position.z - 1f)
+        )
         {
             InfoPanel.loseHearts(damage);
+            health = 0;
             Die();
         }
     }
