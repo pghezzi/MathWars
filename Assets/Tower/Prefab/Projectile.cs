5cdfb33aea59e1bc60feab153800831b6dcd01b6
Momin Humayon <mhumayon@umass.edu>
2024-12-16 00:11:55 -0500

    Projectile generated by Aoe affects all enemies within area

--- Assets/Tower/Prefab/Projectile.cs
+++ Assets/Tower/Prefab/Projectile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -8,6 +9,10 @@ public class Projectile : MonoBehaviour
     private GameObject target;
     private float damage;
     private float velocity;
+
+    private AoeTower source;
+
+    private Boolean areaOfEffect;
 
 
     private void Start()
@@ -23,17 +28,29 @@ public class Projectile : MonoBehaviour
                 velocity * Time.deltaTime * (target.transform.position - transform.position).normalized
             );
         }
-        if((target.transform.position - transform.position).magnitude < 0.1)
-        {
-            target.GetComponent<Enemy>().TakeDamage(damage);
-            Destroy(gameObject);
+        if (!areaOfEffect) {
+                if((target.transform.position - transform.position).magnitude < 0.1)
+            {
+                target.GetComponent<Enemy>().TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        }
+
+        else {
+            if ((target.transform.position - transform.position).magnitude < 0.1) {
+                Collider[] enemiesToHit = Physics.OverlapSphere(transform.position,source.surroundingDistance);
+                enemiesToHit = enemiesToHit.Where(item => item.tag == target.tag).ToArray();
+                Array.ForEach(enemiesToHit,curEnemy => curEnemy.GetComponent<Enemy>().TakeDamage(damage));
+            }
         }
     }
 
-    public void SetupProjectile(GameObject target, float damage, float velocity)
+    public void SetupProjectile(GameObject target, float damage, float velocity, AoeTower source = null)
     {
         this.damage = damage;
         this.velocity = velocity;
         this.target = target;
+        this.source = source;
+        this.areaOfEffect = source != null;
     }
 }
