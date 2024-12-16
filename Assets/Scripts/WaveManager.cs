91281f0cfe5ee8f812b4982e607803abf9d9567d
Ahmad Shahrour <ashahrourr@gmail.com>
2024-12-17 02:40:28 +0300

    fixes

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -9,7 +9,7 @@ public class WaveManager : MonoBehaviour
     public GameObject flyingEnemyPrefab;   // vulture Enemy prefab
     public GameObject heavyEnemyPrefab;    // bear Enemy prefab
     public Transform spawnPoint;           // Starting point
-    public List<Transform> endPoints;             // Target point
+    public Transform endPoint;             // Target point
 
     public Level level;                    // Reference to the Level class
     public int levelDifficulty;
@@ -133,7 +133,6 @@ public class WaveManager : MonoBehaviour
         //Increase health and speed by 1 every wave
         enemyScript.speed += (wave - 1);
         enemyScript.health += (wave - 1);
-        Transform chosenEndPoint = endPoints[UnityEngine.Random.Range(0, endPoints.Count)];
 
         // Convert spawn and end points to grid coordinates
         int blockSize = level.block_size;
@@ -142,13 +141,13 @@ public class WaveManager : MonoBehaviour
             Mathf.RoundToInt(spawnPoint.position.z / blockSize)
         );
         Vector2Int end = new Vector2Int(
-            Mathf.RoundToInt(chosenEndPoint.position.x / blockSize),
-            Mathf.RoundToInt(chosenEndPoint.position.z / blockSize)
+            Mathf.RoundToInt(endPoint.position.x / blockSize),
+            Mathf.RoundToInt(endPoint.position.z / blockSize)
         );
 
         // Calculate the path
         enemyScript.SetPath(CalculatePath(start, end));
-        enemyScript.SetEndPoint(chosenEndPoint);
+        enemyScript.SetEndPoint(endPoint);
     }
 
     List<Vector3> CalculatePath(Vector2Int start, Vector2Int end)
